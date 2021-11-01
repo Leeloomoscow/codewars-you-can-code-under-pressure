cf7d7d7da0eab5d4cf5138af86d442212fc10a33
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-11-01 18:52:09 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,27 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class MyTest {
     @Test
-    void name() {
+    public final void test_all() {
+        Assert.assertEquals("Nope!", 4, Java.doubleInteger(2)); // Test cases here!
+    }
 
+    private static class Assert {
+        public static void assertEquals(String s, int i, Object doubleInteger) {
+
+        }
+    }
+
+    private static class Java {
+        private static int i;
+
+        public static int doubleInteger(int i) {
+            int result = 2;
+            result = result * i;
+            return result; // или return i*2;
+        }
     }
 }
+
